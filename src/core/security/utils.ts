0cfe471fa66a8292d7d6bccacb1621f17b3f3a3d
FAL <falworks.contact@gmail.com>
2023-03-16 11:10:25 +0900

    Remove unused util module

--- src/core/security/utils.ts
+++
@@ -1,8 +0,0 @@
-import { MD5, wordArrayToBytes } from 'src/utils/crypt';
-
-/*
- * Generate MD5 hash bytes from any arbitrary string.
- */
-export function getHashBytesMD5(s: string): Uint8Array {
-  return wordArrayToBytes(MD5(s));
-}
